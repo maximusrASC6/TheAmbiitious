d820bfe95e70ce4dda60353f37336b25fa0a8680
maximusrASC6 <max.ultimaterealm@gmail.com>
2019-07-21 22:01:16 -0400

    Space Invader Update

--- moonSever.js
+++ moonSever.js
@@ -1,13 +1,17 @@
+
+ 
  class MoonSever {
      constructor() {
          this.x = 100
-         this.y = 350
+         this.y = height/2 
         
          this.show = function() {
-            fill(225) | rect(this.x, this.y, 20, 20);
+            fill(225) 
+            rect(this.x, this.y, 40, 40);
           
          this.move = function(dir) {
-             this.y += dir
+             this.y += dir*5;
+    
             }
          
         
@@ -16,7 +20,13 @@
 }
 
 class Invader {
-    constructor() {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
 
+        this.show = function() {
+            fill(127,0,0)
+            circle(this.x, this.y, 40)
+        }
     }
 }
--- sketch.js
+++ sketch.js
@@ -1,11 +1,19 @@
 var ship;
+var enemies = [];
 var boxPosX = 1155
 var boxPosY = 650
+var x = 100
+var y = 350
 
 
 function setup() {
     createCanvas(1230, 700);
+    
     ship = new MoonSever();
+    
+    for (let i = 0; i < 10; i++) {
+        enemies[i]= new Invader(random(height), 150);  
+    }
 }
 
 function draw() {
@@ -15,21 +23,42 @@ function draw() {
     rect(boxPosX, boxPosY, 41, 41, 10)
     //Player's ship
     ship.show();
-    if (keyIsDown(DOWN_ARROW)) {
-        y -= 5;
+    //Enemy
+    for (let i = 0; i < 10; i++) {
+        enemies[i].show();
     }
+    
+    
+      if (keyIsDown(DOWN_ARROW)) {
+          y += 3;
+      }
+      if (keyIsDown(UP_ARROW)) {
+          y -= 3;
+      }
+      if (keyIsDown(RIGHT_ARROW)) {
+          x += 3;
+      }
+      if (keyIsDown(LEFT_ARROW)) {
+          x -= 3;
+      }
+
+      rect(x, y, 40, 40)
+       
+
 
 }
 
-function keyPressed() {
-    if (keyCode === UP_ARROW) {
-        ship.move(-3);
-    } else if (keyCode === DOWN_ARROW) {
-        ship.move(3);
+ function keyPressed() { 
+     if (keyCode === UP_ARROW) {
+         ship.move(-3)
 
-    }
+     } else if (keyCode === DOWN_ARROW) { 
+                ship.move(3)
+            }
 
-}
+     
+
+ }
 
 function mousePressed() {
     if (mouseX >= boxPosX && mouseY >= boxPosY) {
